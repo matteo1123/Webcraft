1f0c26bab20c2c62080539f11d973b1fff988f5a
Matthew <matteo1123@yahoo.com>
2020-01-14 01:51:43 +0000

    Updates

---
+++ precache-manifest.64e9297a38e0bfbe606ed848f0ec30d7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e308dbef82baaac477113ff737f6d82",
+    "url": "/Webcraft/index.html"
+  },
+  {
+    "revision": "6bafed69912b5f482e36",
+    "url": "/Webcraft/static/css/main.f16319f2.chunk.css"
+  },
+  {
+    "revision": "4a2aa11085f5c68310d4",
+    "url": "/Webcraft/static/js/2.8cbb17fd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Webcraft/static/js/2.8cbb17fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6bafed69912b5f482e36",
+    "url": "/Webcraft/static/js/main.da181f34.chunk.js"
+  },
+  {
+    "revision": "1c4865b9ef9cfd23d6c8",
+    "url": "/Webcraft/static/js/runtime-main.bd9593c8.js"
+  },
+  {
+    "revision": "1da2f8a82f37ad1980036afd79666fa5",
+    "url": "/Webcraft/static/media/Building.1da2f8a8.png"
+  },
+  {
+    "revision": "19d3d6dd9a64ab834d2361ce41071898",
+    "url": "/Webcraft/static/media/BuildingCommandBar.19d3d6dd.jpg"
+  },
+  {
+    "revision": "20adb206862f6d87e2c80a2d908fe0bd",
+    "url": "/Webcraft/static/media/Worker.20adb206.png"
+  },
+  {
+    "revision": "f3d75a194b8f7e1769510c48fa161e77",
+    "url": "/Webcraft/static/media/WorkerCommandBar.f3d75a19.jpg"
+  },
+  {
+    "revision": "a41b532f9a99ec6b21f1bd290eadcea1",
+    "url": "/Webcraft/static/media/background.a41b532f.jpg"
+  }
+]);
